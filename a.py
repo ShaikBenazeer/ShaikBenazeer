43f27934fd6950f9b123859b2c9442f24248cca2
ShaikBenazeer <109638534+ShaikBenazeer@users.noreply.github.com>
2022-08-26 23:35:12 -0700

    Update a.py

--- a.py
+++ a.py
@@ -8,3 +8,15 @@ for i in range(len(a)):
            a[i]=t
         j=+1
 print(a)
+
+
+a=[-1,-2,3,-6,-3,3]
+j=0
+for i in range(len(a)):
+    if a[i]<0:
+        if i!=j:
+           t=a[j]
+           a[j]=a[i]
+           a[i]=t
+        j=+1
+print(a)
